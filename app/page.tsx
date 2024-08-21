1a10678dd14ce20005d29ba93e2abbc4790e1c9f
swaraj <syadav482@outlook.com>
2024-08-20 22:13:40 -0400

    Add pfp to landing page

--- app/page.tsx
+++ app/page.tsx
@@ -7,11 +7,19 @@ import {
   Divider,
   Group,
   Center,
-  Tabs,
+  Space,
+  NavLink,
+  Image,
 } from "@mantine/core";
 import { ToggleColorSchemeButton } from "./components/ToggleColorSchemeButton";
 import LinkedinButton from "./components/LinkedinButton";
 import GithubButton from "./components/GithubButton";
+import {
+  IconBriefcase,
+  IconFileCode,
+  IconSchool,
+  IconUser,
+} from "@tabler/icons-react";
 
 export default function HomePage() {
   return (
@@ -29,13 +37,49 @@ export default function HomePage() {
           <LinkedinButton />
           <GithubButton />
           <Title>Hi! I&apos;m Swaraj</Title>
+          <Space />
+          <Image
+            alt="A picture of me"
+            src="/selfie_smaller.png"
+            h={100}
+            w="auto"
+            fit="contain"
+            style={{ borderRadius: "50%" }}
+          />
         </Group>
 
         <Divider />
         <Text>
           I&apos;m a senior at the University of Michigan studying computer
-          science in my last semester.
+          science.
         </Text>
+
+        <Group>
+          <NavLink
+            href=""
+            label="About me"
+            leftSection={<IconUser size="1.5rem" stroke={1.5} />}
+            variant="subtle"
+          />
+          <NavLink
+            href=""
+            label="Education"
+            leftSection={<IconSchool size="1.5rem" stroke={1.5} />}
+            variant="subtle"
+          />
+          <NavLink
+            href=""
+            label="Experiences and Career"
+            leftSection={<IconBriefcase size="1.5rem" stroke={1.5} />}
+            variant="subtle"
+          />
+          <NavLink
+            href=""
+            label="Projects"
+            leftSection={<IconFileCode size="1.5rem" stroke={1.5} />}
+            variant="subtle"
+          />
+        </Group>
       </Stack>
     </Center>
   );
